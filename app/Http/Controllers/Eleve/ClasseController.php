9bc56896151e00758fbd44210195d69858473bc0
masangana <alexmasangana@gmail.com>
2023-03-07 16:00:46 +0300

    View user bulletin

--- app/Http/Controllers/Eleve/ClasseController.php
+++ app/Http/Controllers/Eleve/ClasseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Eleve;
 use App\Http\Controllers\Controller;
 use App\Models\Classe;
 use App\Models\Classe_eleves;
+use App\Models\Epreuve;
+use App\Models\Periode;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 class ClasseController extends Controller
 {
     public function show($id) {
-        $eleve = User::with('hasCoteByCursus')->findOrFail(Auth::user()->id);
+
         $classe = Classe::with(['cours' => function ($q){
             $q->with(['archivedPeriode', 'professeurs'=> function ($q){
                 $q->with('personne');
@@ -21,11 +23,19 @@ class ClasseController extends Controller
 
         $lesClasses = Classe_eleves::with('classe')
                     ->where('user_id', Auth::user()->id)->get();
-        return $lesClasses;
+        
+        $eleve = User::with(['personne', 'hasCoteByCursus' => function ($q){
+                        $q->with('epreuve', 'periode');
+                    }, 'isPupil'])->findOrFail(Auth::user()->id);
+        $epreuves = Epreuve::all();
+        $periodes = Periode::all();
+        //return $eleve;
         return view('eleve.classe.show', [
             'classe' => $classe,
             'eleve' => $eleve,
             'lesClasses' => $lesClasses,
+            'epreuves' => $epreuves,
+            'periodes' => $periodes,
         ]);
     }
 }
